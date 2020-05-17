d58280f06d8f44541681343e348fb31dcf167aed
vishnu <srivishnu995@gmail.com>
2020-05-17 21:28:09 +0530

    added working dropdowm

--- src/component/Select.js
+++ src/component/Select.js
@@ -1,15 +1,28 @@
 import React from 'react'
-import { Select, InputLabel, Box } from '@material-ui/core'
+import { Select, InputLabel, Box, withStyles } from '@material-ui/core'
+import { green } from '@material-ui/core/colors'
 
+
+const useStyles = (style) => ({
+    select: {
+        width: 200,
+        marginLeft: '3%',
+    }, selectLabel: {
+        marginLeft: '3%',
+        marginTop: style.spacing(4)
+    }
+
+})
 
 const SelectList = (props) => {
 
 
-    const { label, handleChange, list, value } = props
+
+    const { label, handleChange, name, list, value, classes } = props
     return (
         <Box>
-            <InputLabel>{label}</InputLabel>
-            <Select onChange={handleChange} value={value}>
+            <InputLabel className={classes.selectLabel}>{label}</InputLabel >
+            <Select name={name} className={classes.select} onChange={handleChange} value={value}>
                 {list}
             </Select>
         </Box>
@@ -19,4 +32,4 @@ const SelectList = (props) => {
 }
 
 
-export default SelectList
+export default (withStyles)(useStyles)(SelectList)
--- src/container/Signup.js
+++ src/container/Signup.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Typography, TextField, Button, Paper, withStyles, Grid, MenuItem } from '@material-ui/core'
 import { getData } from '../utility/api'
 import SelectList from '../component/Select'
-
+import { useHistory } from 'react-router-dom'
 
 const useStyles = (style) => ({
     root: {
@@ -14,6 +14,9 @@ const useStyles = (style) => ({
         margin: style.spacing(10),
         padding: style.spacing(25),
         borderRadius: '10px',
+    },
+    itemList: {
+        marginTop: style.spacing(8)
     }
 })
 
@@ -22,7 +25,7 @@ class Signup extends React.Component {
 
     constructor() {
         super()
-        this.state = { roles: "", role: "", department: "", dept: "" }
+        this.state = { roles: "", role: '', department: "", dept: '' }
     }
 
 
@@ -33,8 +36,8 @@ class Signup extends React.Component {
 
     regData = (res) => {
         if (res.status === 'sucess') {
-            const dept = res.dept[0].department.map((d) => <MenuItem key={d.dept_no} value={d.dept_no} >{d.dept_name}</MenuItem>)
-            const role = res.roles[0].roles.map((r) => <MenuItem key={r.des_no} value={r.des_no} >{r.designation}</MenuItem>)
+            const dept = res.dept[0].department.map((d) => <MenuItem className={this.props.classes.itemList} key={d.dept_no} value={d.dept_no} >{d.dept_name}</MenuItem>)
+            const role = res.roles[0].roles.map((r) => <MenuItem className={this.props.classes.itemList} key={r.des_no} value={r.des_no} >{r.designation}</MenuItem>)
 
             this.setState({ department: dept, roles: role })
         }
@@ -42,13 +45,19 @@ class Signup extends React.Component {
             console.log(res)
     }
 
+    handleChange = (e) => {
+        const { value, name } = e.target
+        this.setState({ [name]: value })
+    }
+
 
 
 
     render() {
-
+        const history = useHistory()
         const { roles, department, dept, role } = this.state
         const { classes } = this.props
+
 
         return (
             <Grid container xs={12} justify="flex-end">
@@ -57,12 +66,12 @@ class Signup extends React.Component {
                         <Typography className={classes.root} variant="h5" align="center">Create Employee</Typography>
                         <TextField className={classes.root} label="Employee Name" />
                         <TextField className={classes.root} label="Employee No" />
-                        <SelectList label="Department" handleChange={this.handleChange} value={dept} list={department} />
-                        <SelectList label="Designation" handleChange={this.handleChange} value={role} list={roles} />
+                        <SelectList name="dept" label="Department" handleChange={this.handleChange} value={dept} list={department} />
+                        <SelectList name="role" label="Designation" handleChange={this.handleChange} value={role} list={roles} />
                         <TextField className={classes.root} label="Password" />
                         <TextField className={classes.root} label="Re-Type Password" />
                         <Button className={classes.root} variant="contained" color="primary" onClick={() => { }} >Create</Button>
-                        <Button className={classes.root} color="primary" onClick={() => { }} >Already have account</Button>
+                        <Button className={classes.root} color="primary" onClick={() => { history.push('/signin') }} >Already have account</Button>
                     </Paper>
                 </Grid>
             </Grid>
